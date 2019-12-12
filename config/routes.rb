31af1dd397ef3b9ba140ed66f18d43381a71e753
ryo94152 <ryo94152@gmail.com>
2019-12-12 12:36:31 +0900

    ユーザー登録画面実装,3,4,5
    # What
    ユーザー登録画面実装した
    # Why
    ユーザー登録するため

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "signup#step1"
+  root to: "signup#index"
   resources :signup do
     collection do
       get 'step1'
       get 'step2'
       get 'step3'
-      get 'step4' 
+      get 'step4'
       get 'step5' # ここで、入力の全てが終了する
       get 'done' # 登録完了後のページ
     end
