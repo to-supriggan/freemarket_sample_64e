47b4ef1aa5a7602b622ce215953738a4239ddebc
hiroishi37 <57392511+hiroishi37@users.noreply.github.com>
2019-12-15 15:34:21 +0900

    correct sider again5

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   root to: "products#item_detail"
   resources :signup do
     collection do
-      get "index", to: "signup#index"
-      get "step1", to: "signup#step1"
-      get "step2", to: "signup#step2"
-      get "step3", to: "signup#step3"
-      get "step4", to: "signup#step4"
-      get "step5", to: "signup#step5"
-      get "login", to: "signup#login"
+      get "signup/index", to: "signup#index"
+      get "signup/step1", to: "signup#step1"
+      get "signup/step2", to: "signup#step2"
+      get "signup/step3", to: "signup#step3"
+      get "signup/step4", to: "signup#step4"
+      get "signup/step5", to: "signup#step5"
+      get "signup/login", to: "signup#login"
     end
   end
   # resources :products
