491a3cdf5faba0ca885472c4d48748229b57101e
to-supriggan <era.pew@gmail.com>
2019-12-18 16:37:41 +0900

    @user削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,7 +40,6 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    @user = User.find(current_user.id)
     @comments = @product.comments.order('created_at ASC')
 
     # 出品者の商品に対する評価を確認
