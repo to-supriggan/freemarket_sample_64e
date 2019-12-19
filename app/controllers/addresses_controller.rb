0edbe09f31f22148c62c2e4cdc8a5d07baaee83f
ryo94152 <ryo94152@gmail.com>
2019-12-19 16:18:09 +0900

    ユーザー登録/住所のみ
    What
    インデントの修正
    Why
    お届け先情報を登録するため

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -6,7 +6,7 @@ class AddressesController < ApplicationController
 
   def create
     @address = Address.new(save_params)
-    
+
     if @address.save
       redirect_to step4_signup_index_path 
     else
@@ -17,7 +17,7 @@ class AddressesController < ApplicationController
   private
   def save_params
     params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_cord,
-                                     :prefecture_id, :city, :street_num, :building, :phone_num).merge(user_id: 1)
+                                    :prefecture_id, :city, :street_num, :building, :phone_num).merge(user_id: 1)
   end
 
 
