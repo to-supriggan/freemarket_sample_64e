c99558a9b8e74cb028af9b561b465fbaa860173e
to-supriggan <era.pew@gmail.com>
2019-12-19 19:35:53 +0900

    出品ページ-修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   has_many :comments
-  has_many :images, inverse_of: :product
+  has_many :images, inverse_of: :product, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true
   has_many :goods
   belongs_to :user
   has_one :dealings
