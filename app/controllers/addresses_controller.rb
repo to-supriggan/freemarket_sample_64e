b5214bba2282bb21fc01af534e4a2e420c0dd54d
ryo94152 <ryo94152@gmail.com>
2019-12-19 16:07:08 +0900

    ユーザー登録/住所のみ
    What
    save時の分岐を追記
    Why
    お届け先情報を登録するため

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -6,14 +6,19 @@ class AddressesController < ApplicationController
 
   def create
     @address = Address.new(save_params)
-    @address.save!
-    redirect_to step4_signup_index_path 
+    
+    if @address.save
+      redirect_to step4_signup_index_path 
+    else
+      render "new" 
+    end
   end
 
-def save_params
-  params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_cord,
-                                   :prefecture_id, :city, :street_num, :building, :phone_num).merge(user_id: 1)
-end
+  private
+  def save_params
+    params.require(:address).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_cord,
+                                     :prefecture_id, :city, :street_num, :building, :phone_num).merge(user_id: 1)
+  end
 
 
 
