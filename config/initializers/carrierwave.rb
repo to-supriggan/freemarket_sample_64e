cad7e5e4a3471d45cfe93134a4edacd85f1c2a64
to-supriggan <era.pew@gmail.com>
2019-12-20 20:34:17 +0900

    S3実装-画像保存

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'freemarket_sample_64e'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/adminbuserucket'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
