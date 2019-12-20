4dc0c4db53f34a671ba94d51b70ba89da4402baf
to-supriggan <era.pew@gmail.com>
2019-12-20 17:06:55 +0900

    S3実装-対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,8 +11,8 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'freemarket_sample_64e'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-      aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
     }
   else
