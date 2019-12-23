b4d94feec12344e42566db71bf324d777941c86c
to-supriggan <era.pew@gmail.com>
2019-12-23 19:49:40 +0900

    Revert "売り切れ対応"
    This reverts commit 244bb4c9d3404a8596317693a03de18c79bcfd58.

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -4,8 +4,8 @@ $(document).on('turbolinks:load', function(){
     autoplay:true,
     dots:true,
     arrows:true,
-    prevArrow: '<img src="/icons8-arrow-50.png" class="slide-arrow">',
-    nextArrow: '<img src="/icons8-arrow-50.png" class="next-arrow">',
+    prevArrow: '<img src="icons8-arrow-50.png" class="slide-arrow">',
+    nextArrow: '<img src="icons8-arrow-50.png" class="next-arrow">',
     dotsClass: 'slide-dots',
   });
 });
