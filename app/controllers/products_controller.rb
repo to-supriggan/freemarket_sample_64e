2a288318b932cced56cfe4c9c7021b42d264bec1
ryo94152 <ryo94152@gmail.com>
2019-12-23 15:31:44 +0900

    商品削除機能実装
    # What
    商品の削除
    # Why
    出品商品の削除機能実装のため

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :before_params
+  before_action :set_product, only: [:edit, :show, :destroy]
 
   def new
     @product = Product.new
@@ -47,9 +48,11 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    product = Product.find(params[:id])
-    product.destroy
-    redirect_to root_path
+    if @product.destroy
+      redirect_to root_path
+    else
+      render :show 
+    end
   end
 
   def get_child_category
@@ -71,6 +74,10 @@ class ProductsController < ApplicationController
   end
 
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def before_params
     @category = Category.where(ancestry: nil)
