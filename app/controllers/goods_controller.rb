4318b2768cd477b60a2bd6b754bc1600fe350d1f
to-supriggan <era.pew@gmail.com>
2019-12-25 17:19:40 +0900

    商品詳細画面-いいね機能ボタン修正

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -4,13 +4,10 @@ class GoodsController < ApplicationController
     if product.goods.where(user_id: current_user.id).exists?
       good = current_user.goods.find_by(product_id: product.id)
       good.destroy
-      render json: good.id, status: 200
     else
       good = current_user.goods.new(product_id: product.id)
       good.save!
-      render json: good.id, status: 200
     end
-    bindning.pry
     redirect_to product_path(params[:product_id])
   end
 
